55bd3777141ef8f9e2632ed2b78c80dda6ca511e
naoyashiga <naoyashiga0@gmail.com>
2015-10-03 17:35:34 +0900

    Initial Commit

---
+++ SwiftDesignPatterns/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftDesignPatterns
+//
+//  Created by naoyashiga on 2015/10/03.
+//  Copyright © 2015年 naoyashiga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftDesignPatternsTests/SwiftDesignPatternsTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftDesignPatternsTests.swift
+//  SwiftDesignPatternsTests
+//
+//  Created by naoyashiga on 2015/10/03.
+//  Copyright © 2015年 naoyashiga. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftDesignPatterns
+
+class SwiftDesignPatternsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
